f067c0ca04fcfb300018bea89072edc56c96478c
notequals <hshsiao63@gmail.com>
2020-04-28 15:24:56 -0700

    Initial Commit

---
+++ Assets/Game/Board/BoardManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Game.Board
+{
+    public class BoardManager : MonoBehaviour
+    {
+        Dictionary<Vector2Int, Slot> slots;
+
+        public Transform slotParent;
+        public Slot slotPrefab;
+
+        public float extraColWidth = 50;
+        public float extraRowHeight = 50;
+
+        bool boardInitialized = false;
+
+        void Start()
+        {
+            slots = new Dictionary<Vector2Int, Slot>();
+
+            InitializeBoard();
+        }
+        void InitializeBoard()
+        {
+            slotParent.DeleteChildren();
+
+            var rectTransform = slotPrefab.GetComponent<RectTransform>();
+
+            Slot.slotWidth = rectTransform.rect.width + extraRowHeight;
+            Slot.slotHeight = rectTransform.rect.height + extraColWidth;
+
+            //Slot.extraMaskHeight = extraMaskHeight;
+
+            int maxHeight = 7;
+            int maxWidth = 7;
+
+            for (int y = 0; y < maxHeight; y++)
+            {
+                for (int x = 0; x < maxWidth; x++)
+                {
+                    SpawnSlot(x, y);
+                }
+            }
+
+            InitializeNeighbours();
+
+            boardInitialized = true;
+        }
+
+        void InitializeNeighbours()
+        {
+            foreach(var slot in slots.Values)
+            {
+                AddNeighbours(slot);
+            }
+        }
+
+        void AddNeighbours(Slot slot)
+        {
+            var x = slot.x;
+            var y = slot.y;
+
+            AddNeighbour(slot, x, y - 1); //up
+            AddNeighbour(slot, x, y + 1); //down
+            AddNeighbour(slot, x - 1, y); //left
+            AddNeighbour(slot, x + 1, y); //right
+        }
+
+        void ResetNeighbours(Slot slot)
+        {
+            foreach (var neighbour in slot.neighbours)
+            {
+                if (neighbour != null)
+                {
+                    neighbour.neighbours.Remove(slot);
+                }
+            }
+
+            slot.neighbours = new HashSet<Slot>();
+
+            AddNeighbours(slot);
+        }
+
+        void AddNeighbour(Slot slot, int x, int y)
+        {
+            var neighbour = GetSlot(x, y);
+
+            if (neighbour != null && !slot.neighbours.Contains(neighbour))
+            {
+                slot.neighbours.Add(neighbour);
+                neighbour.neighbours.Add(slot);
+            }
+        }
+        void SpawnSlot(int x, int y)
+        {
+            var newSlot = Instantiate(slotPrefab, slotParent);
+            newSlot.Initialize(x, y);
+
+            slots.Add(new Vector2Int(x, y), newSlot);
+        }
+
+        public Slot GetSlot(int x, int y)
+        {
+            Slot slot;
+            if(slots.TryGetValue(new Vector2Int(x, y), out slot))
+            {
+                return slot;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Game/Board/Slot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Game.Board
+{
+    public class Slot : MonoBehaviour
+    {
+        public static float slotWidth;
+        public static float slotHeight;
+
+        public Image image;
+        public CanvasGroup canvasGroup;
+
+        public HashSet<Slot> neighbours = new HashSet<Slot>();
+
+        public int x;
+        public int y;
+
+        BoardManager boardManager;
+        RectTransform rectTransform;
+
+        [NonSerialized]
+        public FoodObject food;
+
+        public void Initialize(int x, int y)
+        {
+            boardManager = GameManager.instance.boardManager;            
+
+            this.x = x;
+            this.y = y;
+
+            rectTransform = GetComponent<RectTransform>();
+            
+            var randomFood = GameManager.instance.foodManager.GenerateRandomFood();
+
+            SetSlotType(randomFood);
+        }
+        public void SetSlotType(FoodObject food)
+        {
+            this.food = food;
+            image.sprite = food.icon;
+        }
+    }
+}
